a5170560f63342ff0989c622dd1f18cd2ab4bb7f
umair zafar <umair.zafar@mwanmobile.org>
2026-03-30 17:40:58 +0500

    backend design, admin panel

---
+++ database/migrations/2026_03_30_123331_create_education_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('education', function (Blueprint $table) {
+            $table->id();
+            $table->string('degree');
+            $table->string('institution');
+            $table->string('duration')->nullable();
+            $table->text('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('education');
+    }
+};
