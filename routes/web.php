853d33c04188151d72260465d3a283ef51e05184
umair zafar <umair.zafar@mwanmobile.org>
2026-03-31 11:40:11 +0500

    correction

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('fronthome');
 Route::middleware('auth')->prefix('admin')->name('admin.')->group(function () {
 
     Route::get('/', [HomeController::class, 'index'])->name('home');
